671c99039942d24af63a533a362c8ee8eff31ffc
huangmei123 <1593702111@qq.com>
2020-05-21 09:58:37 +0800

    webpack深入

--- webpack/webpack-first/webpack.config.js
+++ webpack/webpack-first/webpack.config.js
@@ -2,6 +2,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const isDev = process.env.NODE_ENV === 'development';
 const config = require('./public/config')[isDev ? 'dev' : 'build'];
+const {CleanWebpackPlugin} =require('clean-webpack-plugin');
 
 module.exports = {
     devtool:'cheap-module-eval-source-map',//开发环境下使用
@@ -16,24 +17,29 @@ module.exports = {
         compress:true//是否启用gzip压缩
     },
     module:{
+        // entry:'./src/index.js',
+        entry:[
+            './src/index.js',
+            './src/polyfills.js'
+        ],
+        output:{
+            path:path.reoslve(__dirname,'dist'),//必须是绝对路径
+            filename:'bundle.[hash].js',
+            publicPath:'/' //通常是CDN地址
+        },
         rules:[
             {
-                test:/\.jsx?$/,
-                use:{
-                    loader:'babel-loader',
-                    options:{
-                        presets:["@babel/preset-env"],
-                        plugins:[
-                            [
-                                "@babel/plugin-transform-runtime",
-                                {
-                                    "corejs":3
-                                }
-                            ]
-                        ]
+                test: /\.(png|jpg|gif|jpeg|webp|svg|eot|ttf|woff|woff2)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10240, //10K
+                            esModule: false 
+                        }
                     }
-                },
-                exclude:/node_modules/ //排除nodule_modules目录
+                ],
+                exclude: /node_modules/
             }
         ]
     },
@@ -48,6 +54,11 @@ module.exports = {
             //     collapseWhitespace: false, //是否折叠空白
             // },
             //hash :true //是否加上hash 默认是false
+        }),
+        //不用传参 它可以找到output
+        new CleanWebpackPlugin({
+            //不删除dll目录下的文件
+            cleanOnceBeforeBuildPatterns:['**/*','!dll','!dll/**']
         })
     ]
 }
